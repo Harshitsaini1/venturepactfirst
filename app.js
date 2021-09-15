2d72e6172bf324f984f4a8d15a5cc75b9795186c
Harshisaini1 <harshitsaini9756@gmail.com>
2021-09-15 16:10:26 +0530

    add Path

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const { error } = require("console");
 const { ConnectionStates } = require("mongoose");
 
 
-app.use(express.static(process.cwd() + "myfirstapp\src\index.html"));
+app.use(express.static(process.cwd() + "myfirstapp/src/index.html"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
